409d06c55a5d9d73cbc3ffd8bb18b542156763dc
KX10Anonymous <kx10anonymous@gmail.com>
2022-12-01 21:17:22 +0200

    Changes to appointments management

--- src/java/main/core/content/AppointmentsManager.java
+++ src/java/main/core/content/AppointmentsManager.java
@@ -12,31 +12,68 @@ import main.core.Appointment;
 import java.util.HashMap;
 import main.core.EAppointmentStatus;
 import sun.util.calendar.LocalGregorianCalendar.Date;
+
 /**
  *
  * @author J-ANONIMO
  */
 public class AppointmentsManager {
-    
+
     private final transient PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
     private final transient VetoableChangeSupport vetoableChangeSupport = new VetoableChangeSupport(this);
     public static final String PROP_APPOINTMENTSAL = "appointmentsAL";
     public static final String PROP_APPOINTMENSHASHMAP = "appointmensHashMap";
-    
-    protected ArrayList<Appointment> appointmentsAL;
-    protected HashMap<Integer,Appointment> appointmensHashMap;
-    protected HashMap<EAppointmentStatus, Appointment> appointmentStatus;
-    protected HashMap<Date, Appointment> appointmentsDates;
-    
+
+    private ArrayList<Appointment> appointmentsAL;
+    protected HashMap<Integer, Appointment> appointmentsHashMap;
+    protected HashMap<EAppointmentStatus, Appointment> appointmentStatusHashMap;
+    protected HashMap<Date, Appointment> appointmentsDateHashMap;
+    protected EListRequest request;
+
+    /**
+     *
+     * @param request
+     */
+    public AppointmentsManager(EListRequest request) {
+
+        if (null == request) {
+            //Choose Array List in case request is null
+            appointmentsAL = new ArrayList<>();
+        } else { //Create array list if request is made for appointments arraylist
+            switch (request) {
+                case ARRAY_LIST:
+                    appointmentsAL = new ArrayList<>();
+                    break;
+                case DATE:
+                    //Create hash map for appointments date.
+                    appointmentsDateHashMap = new HashMap<>();
+                    break;
+                case HASH_MAP:
+                    //Create Hash Map for appointment, user uses this to pull all appointments
+                    appointmentsHashMap = new HashMap<>();
+                    break;
+                default:
+                    appointmentStatusHashMap = new HashMap<>();
+                    break;
+            }
+            this.request = request;
+        }
+    }
+
     /**
      * @return the appointmentsAL
      */
     public ArrayList<Appointment> getAppointmentsAL() {
-        return appointmentsAL;
+        if (getRequest() == EListRequest.ARRAY_LIST) {
+            return appointmentsAL;
+        } else {
+            return new ArrayList<>();
+        }
+
     }
 
     /**
-     * @param appointmentsAL the appointmentsAL to set
+     * @param appointmentsAL the appointmentsAL to set.
      * @throws java.beans.PropertyVetoException
      */
     public void setAppointmentsAL(ArrayList<Appointment> appointmentsAL) throws PropertyVetoException {
@@ -47,31 +84,144 @@ public class AppointmentsManager {
     }
 
     /**
-     * @return the appointmensHashMap
+     * @return the appointmentsHashMap
      */
-    public HashMap<Integer,Appointment> getAppointmensHashMap() {
-        return appointmensHashMap;
+    public HashMap<Integer, Appointment> getAppointmensHashMap() {
+        if (getRequest() == EListRequest.HASH_MAP) {
+            return getAppointmentsHashMap();
+        } else {
+            return new HashMap<>();
+        }
     }
 
     /**
-     * @param appointmensHashMap the appointmensHashMap to set
+     * @param appointmensHashMap the appointmentsHashMap to set.
      * @throws java.beans.PropertyVetoException
      */
-    public void setAppointmensHashMap(HashMap<Integer,Appointment> appointmensHashMap) throws PropertyVetoException {
-        java.util.HashMap<java.lang.Integer, main.core.Appointment> oldAppointmensHashMap = this.appointmensHashMap;
+    public void setAppointmensHashMap(HashMap<Integer, Appointment> appointmensHashMap) throws PropertyVetoException {
+        java.util.HashMap<java.lang.Integer, main.core.Appointment> oldAppointmensHashMap = this.getAppointmentsHashMap();
         vetoableChangeSupport.fireVetoableChange(PROP_APPOINTMENSHASHMAP, oldAppointmensHashMap, appointmensHashMap);
-        this.appointmensHashMap = appointmensHashMap;
+        this.setAppointmentsHashMap(appointmensHashMap);
         propertyChangeSupport.firePropertyChange(PROP_APPOINTMENSHASHMAP, oldAppointmensHashMap, appointmensHashMap);
     }
-    
+
     /**
-     * Add appointment to the Status hash map, will help pulling appointments based on status
+     * Add appointment to the Status hash map, will help pulling appointments.
+     * based on status
+     *
      * @param status
+     * @param appointment
+     * @return
+     */
+    public boolean addAppointmenByStatus(EAppointmentStatus status, Appointment appointment) {
+        if (appointment != null) {
+            appointmentStatusHashMap.put(status, appointment);
+        }
+        return true;
+    }
+
+    /**
+     * Utility method to check if the appointment is NULL or not.
      * @param appointment
      * @return 
      */
-    public boolean addAppointmenByStatus(EAppointmentStatus status, Appointment appointment){
+    private boolean isAppointmentNull(Appointment appointment){
+        return appointment == null;
+    }
+    
+    /**
+     * Adds appointment to hash map by date.
+     * @param date
+     * @param appointment
+     * @return 
+     */
+    public boolean addAppointmentsByDate(Date date, Appointment appointment){
+        if(!isAppointmentNull(appointment)){
+            appointmentsDateHashMap.put(date, appointment);
+        }
         return true;
     }
     
+    /**
+     * Adds appointment to hash map using ID key.
+     * @param id
+     * @param appointment
+     * @return 
+     */
+    public boolean addAppointmentToHashMap(int id, Appointment appointment){
+        if(!isAppointmentNull(appointment)){
+            appointmentsHashMap.put(id, appointment);
+        }
+        return true;
+    }
+    
+    /**
+     * @return the appointmentsHashMap
+     */
+    public HashMap<Integer, Appointment> getAppointmentsHashMap() {
+        if(request == EListRequest.HASH_MAP){
+            return appointmentsHashMap;
+        }else{
+            return new HashMap<>();
+        }   
+    }
+
+    /**
+     * @param appointmentsHashMap the appointmentsHashMap to set
+     */
+    public void setAppointmentsHashMap(HashMap<Integer, Appointment> appointmentsHashMap) {
+        this.appointmentsHashMap = appointmentsHashMap;
+    }
+
+    /**
+     * @return the appointmentStatusHashMap
+     */
+    public HashMap<EAppointmentStatus, Appointment> getAppointmentStatusHashMap() {
+        if (request == EListRequest.STATUS) {
+            return appointmentStatusHashMap;
+        } else {
+            return new HashMap<>();
+        }
+    }
+
+    /**
+     * @param appointmentStatusHashMap the appointmentStatusHashMap to set
+     */
+    public void setAppointmentStatusHashMap(HashMap<EAppointmentStatus, Appointment> appointmentStatusHashMap) {
+        this.appointmentStatusHashMap = appointmentStatusHashMap;
+    }
+
+    /**
+     * @return the appointmentsDateHashMap
+     */
+    public HashMap<Date, Appointment> getAppointmentsDateHashMap() {
+        if (request == EListRequest.DATE) {
+            return appointmentsDateHashMap;
+        } else {
+            return new HashMap<>();
+        }
+
+    }
+
+    /**
+     * @param appointmentsDateHashMap the appointmentsDateHashMap to set
+     */
+    public void setAppointmentsDateHashMap(HashMap<Date, Appointment> appointmentsDateHashMap) {
+        this.appointmentsDateHashMap = appointmentsDateHashMap;
+    }
+
+    /**
+     * @return the request
+     */
+    public EListRequest getRequest() {
+        return request;
+    }
+
+    /**
+     * @param request the request to set
+     */
+    public void setRequest(EListRequest request) {
+        this.request = request;
+    }
+
 }
